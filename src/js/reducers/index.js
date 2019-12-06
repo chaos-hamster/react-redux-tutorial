863abd7c44a4fa03904e14407995bfbdbc536dcd
Mark Sweeney <mark@ninereeds.co.uk>
2019-12-06 07:39:23 +0000

    Added thunk to Fetch async action

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,7 +1,8 @@
-import { ADD_ARTICLE } from "../constants/action-types";
+import { ADD_ARTICLE, DATA_LOADED } from "../constants/action-types";
 
 const initialState = {
-    articles: []
+    articles: [],
+    remoteArticles: []
   };
 
 function rootReducer(state = initialState, action) {
@@ -11,6 +12,12 @@ function rootReducer(state = initialState, action) {
           });
     }
     
+    if (action.type === DATA_LOADED) {
+      return Object.assign({}, state, {
+        remoteArticles: state.remoteArticles.concat(action.payload)
+      });
+    }
+
     return state;
 };
 
